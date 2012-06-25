49ab0bc96c8f3f35937e642369c3c898ef65888c
tsHan <tshan@innopnt.com>
2012-06-25 15:40:11 +0900

    test commit 3

--- src/com/git/test/Git_Test.java
+++ src/com/git/test/Git_Test.java
@@ -1,10 +1,14 @@
 package com.git.test;
 
 import android.app.Activity;
+import android.widget.Toast;
 import android.os.Bundle;
+import android.content.Context;
 
 public class Git_Test extends Activity
-{
+{	
+	Context gitContext;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -12,6 +16,8 @@ public class Git_Test extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        gitContext = this;
         
+        Toast.makeText(gitContext, "Git 테스트 중!", 0).show();
     }
 }
